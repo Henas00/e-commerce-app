380a14ef8ace1ce77a17eade9e08ef01bfc2039c
Henas <henas.00@gmail.com>
2022-03-13 18:19:50 +0100

    homepage into smaller components, styling menu items.

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import Menu from '../../components/menu/menu.component';
+import './homepage.styles.scss'
+
+const HomePage = () => {
+    return (
+      <div className="homepage">
+           <Menu />
+      </div>
+    );
+}
+ 
+export default HomePage;
